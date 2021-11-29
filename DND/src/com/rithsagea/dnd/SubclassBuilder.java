c948373515f174ee124be34e47cff63424654185
Rithsagea Aquadom <rithsagea@gmail.com>
2021-11-29 14:06:09 -0500

    druid done

--- DND/src/com/rithsagea/dnd/SubclassBuilder.java
+++ DND/src/com/rithsagea/dnd/SubclassBuilder.java
@@ -568,6 +568,48 @@ public class SubclassBuilder {
 		return subclass;
 	}
 	
+	private static DndSubclass createMoon() {
+		DndSubclass subclass = new DndSubclass();
+		subclass.id = "moon";
+		subclass.name = "Moon";
+		subclass.flavor = "Druid Circle";
+		subclass.classId = "druid";
+		subclass.description = StringUtil.convertDesc(
+				"Druids of the Circle of the Moon are fierce guardians of the wilds. Their order gathers under the full moon to share news and trade warnings. They haunt the deepest parts of the wilderness, where they might go for weeks on end before crossing paths with another humanoid creature, let alone another druid.\r\n"
+				+ "\r\n"
+				+ "Changeable as the moon, a druid of this circle might prowl as a great cat one night, soar over the treetops as an eagle the next day, and crash through the undergrowth in bear form to drive off a trespassing monster. The wild is in the druid's blood.");
+		
+		subclass.levels = new ArrayList<>(Collections.nCopies(20, null));
+		
+		_classId = subclass.classId;
+		_subclassId = subclass.id;
+		
+		createLevel(subclass, 2, "combat-wild-shape", "circle-forms");
+		registerFeature("combat-wild-shape", "Combat Wild Shape",
+				"When you choose this circle at 2nd level, you gain the ability to use Wild Shape on your turn as a bonus action, rather than as an action.\r\n"
+				+ "\r\n"
+				+ "Additionally, while you are transformed by Wild Shape, you can use a bonus action to expend one spell slot to regain 1d8 hit points per level of the spell slot expended.");
+		
+		registerFeature("circle-forms", "Circle Forms",
+				"The rites of your circle grant you the ability to transform into more dangerous animal forms. Starting at 2nd level, you can use your Wild Shape to transform into a beast with a challenge rating as high as 1. You ignore the Max. CR column of the Beast Shapes table, but must abide by the other limitations there.\r\n"
+				+ "\r\n"
+				+ "Starting at 6th level, you can transform into a beast with a challenge rating as high as your druid level divided by 3, rounded down.");
+		
+		createLevel(subclass, 6, "primal-strike");
+		registerFeature("primal-strike", "Primal Strike",
+				"Starting at 6th level, your attacks in beast form count as magical for the purpose of overcoming resistance and immunity to nonmagical attacks and damage.");
+		
+		createLevel(subclass, 10, "elemental-wild-shape");
+		registerFeature("elemental-wild-shape", "Elemental Wild Shape",
+				"At 10th level, you can expend two uses of Wild Shape at the same time to transform into an air elemental, an earth elemental, a fire elemental, or a water elemental.");
+		
+		createLevel(subclass, 14, "thousand-forms");
+		registerFeature("thousand-forms", "Thousand Forms",
+				"By 14th level, you have learned to use magic to alter your physical form in more subtle ways. You can cast the Alter Self spell at will.");
+		
+		return subclass;
+	}
+	
 	/*
 	template
 	
@@ -611,6 +653,7 @@ public class SubclassBuilder {
 		book.register(createWar());
 		
 		book.register(createLand());
+		book.register(createMoon());
 		SourceRegistry.saveBooks();
 	}
 }
